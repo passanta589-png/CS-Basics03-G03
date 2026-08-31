4712a8b3c654d7fa194b49c98b7c139aa83fac72
passant ahmed <passantahmed992006@outlook.com>
2026-08-31 03:39:31 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
 #region 01
-int pages = 464;
-bool isAvailable = true;
-if (pages > 300 && isAvailable)
-{
-   Console.WriteLine("you can borrow this book");
-}
+//int pages = 464;
+//bool isAvailable = true;
+//if (pages > 300 && isAvailable)
+//{
+//    Console.WriteLine("you can borrow this book");
+//}
 
 #endregion
